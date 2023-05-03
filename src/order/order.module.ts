01276d4eabd7a5ae67f488eb25be4691ffd290f4
Talwinder Singh <singhtalwinder790@gmail.com>
2023-05-03 13:14:39 +0530

    feat: add order module and service

---
+++ src/order/order.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { OrderService } from './order.service';
+import { OrderResolver } from './order.resolver';
+import { ProductModule } from 'src/product/product.module';
+
+@Module({
+  providers: [OrderResolver, OrderService],
+  imports: [ProductModule]
+})
+export class OrderModule {}
