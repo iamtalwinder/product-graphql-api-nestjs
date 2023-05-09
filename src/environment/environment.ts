9bdb5e5957995f554e08fbdffef5e5cf96c1577a
Talwinder Singh <singhtalwinder790@gmail.com>
2023-05-09 15:40:16 +0530

    feat: add environment

---
+++ src/environment/environment.ts
@@ -0,0 +1,25 @@
+import * as dotenv from 'dotenv';
+import { plainToInstance } from 'class-transformer';
+import { validateSync } from 'class-validator';
+import { EnvironmentDto } from './environment.dto';
+import { EnvironmentInterface } from './environment.interface';
+
+dotenv.config();
+const env: NodeJS.ProcessEnv = process.env;
+
+function validateEnvironment(plainEnv: EnvironmentInterface): EnvironmentDto {
+  const envDto = plainToInstance(EnvironmentDto, plainEnv);
+
+  const errors = validateSync(envDto);
+  if (errors.length > 0) {
+    throw new Error(`Configuration validation error: ${errors}`);
+  }
+
+  return envDto;
+}
+
+const environment: EnvironmentInterface = {
+  port: parseInt(env.PORT),
+};
+
+export default validateEnvironment(environment);
