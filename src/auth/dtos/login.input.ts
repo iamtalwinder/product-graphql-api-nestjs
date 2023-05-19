80c4bae881cb4b1ff8bae791c0abac22ac5d38f5
Talwinder Singh <singhtalwinder790@gmail.com>
2023-05-19 14:52:16 +0530

    feat: add login input

---
+++ src/auth/dtos/login.input.ts
@@ -0,0 +1,14 @@
+import { InputType, Field } from '@nestjs/graphql';
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+@InputType()
+export class LoginInput {
+  @Field()
+  @IsEmail()
+  email: string;
+
+  @Field()
+  @IsString()
+  @MinLength(6)
+  password: string;
+}
