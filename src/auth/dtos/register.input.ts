c02c668881f4b4c72b1e24dbd985b2dbf5961dfc
Talwinder Singh <singhtalwinder790@gmail.com>
2023-05-20 14:52:16 +0530

    feat: add register input

---
+++ src/auth/dtos/register.input.ts
@@ -0,0 +1,22 @@
+import { InputType, Field } from '@nestjs/graphql';
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+@InputType()
+export class RegisterInput {
+  @Field()
+  @IsEmail()
+  email: string;
+
+  @Field()
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @Field()
+  @IsString()
+  firstName: string;
+
+  @Field()
+  @IsString()
+  lastName: string;
+}
