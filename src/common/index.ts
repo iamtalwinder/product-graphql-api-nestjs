f8937c34cdf4b825e395ce507297dc2203604e67
Talwinder Singh <singhtalwinder790@gmail.com>
2023-05-27 13:38:16 +0530

    feat: add translations key

--- src/common/index.ts
+++ src/common/index.ts
@@ -2,3 +2,4 @@ export * from './base.entity';
 export * from './base.service';
 export * from './encryption';
 export * from './graphql';
+export * from './translation-keys';
---
+++ src/common/translation-keys.ts
@@ -0,0 +1,7 @@
+export const translationKeys = {
+  auth: {
+    invalidCredentials: 'auth.invalid-credentials',
+    userNotFound: 'auth.user-not-found',
+    emailAlreadyExists: 'auth.email-already-exists',
+  },
+};
