fe8023a58e36b67a6cd319fbc72bb0588a8a82ac
Talwinder Singh <singhtalwinder790@gmail.com>
2023-05-28 13:38:16 +0530

    feat: refactor auth service to use translation keys

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@nestjs/common';
-import { EncryptionService } from 'src/common/encryption';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { translationKeys, EncryptionService } from 'src/common';
 import { UserDocument, UserRole, UserService } from 'src/user';
 import { AuthTokenOutput, LoginInput, RegisterInput } from '../dtos';
 import { TokenService } from './token.service';
@@ -15,6 +15,13 @@ export class AuthService {
     const { email, password, firstName, lastName } = dto;
 
     const hashedPassword = await EncryptionService.hash(password);
+    const userExists: boolean =
+      await this.userService.doesUserWithEmailExist(email);
+
+    if (userExists) {
+      throw new BadRequestException(translationKeys.auth.emailAlreadyExists);
+    }
+
     const user: UserDocument = await this.userService.create({
       email,
       firstName,
@@ -32,15 +39,24 @@ export class AuthService {
 
   async login(dto: LoginInput): Promise<AuthTokenOutput> {
     const { email, password } = dto;
-
     const user = await this.userService.findOne({ email });
-    if (user && (await EncryptionService.compare(password, user.password))) {
-      return this.tokenService.issueToken({
-        id: user.id,
-        email: user.email,
-        role: user.role,
-      });
+
+    if (!user) {
+      throw new BadRequestException(translationKeys.auth.invalidCredentials);
     }
-    throw new Error('Invalid credentials');
+
+    const isPasswordValid = await EncryptionService.compare(
+      password,
+      user.password,
+    );
+    if (!isPasswordValid) {
+      throw new BadRequestException(translationKeys.auth.invalidCredentials);
+    }
+
+    return this.tokenService.issueToken({
+      id: user.id,
+      email: user.email,
+      role: user.role,
+    });
   }
 }
