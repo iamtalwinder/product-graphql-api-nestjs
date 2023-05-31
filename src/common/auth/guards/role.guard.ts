280ccaf16fe5f65f38224eb9775a5753b0206949
Talwinder Singh <singhtalwinder790@gmail.com>
2023-05-31 14:19:45 +0530

    feat: add role guard

---
+++ src/common/auth/guards/role.guard.ts
@@ -0,0 +1,43 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { Reflector } from '@nestjs/core';
+import { translationKeys } from 'src/common';
+
+const INSUFFICIENT_PERMISSION: string =
+  translationKeys.auth.insufficientPermission;
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.get<string[]>(
+      'roles',
+      context.getHandler(),
+    );
+
+    console.log(requiredRoles)
+    if (!requiredRoles) {
+      return true; // If no specific roles are required, allow access.
+    }
+
+    const ctx = GqlExecutionContext.create(context);
+    const { user } = ctx.getContext().req;
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    const hasRole = requiredRoles.some((role) => user.roles?.includes(role));
+    if (!hasRole) {
+      throw new UnauthorizedException(INSUFFICIENT_PERMISSION);
+    }
+
+    return true;
+  }
+}
