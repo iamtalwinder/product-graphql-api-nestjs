c7264f49da615cc54aaa63c69aa44e8697c928db
Talwinder Singh <singhtalwinder790@gmail.com>
2023-06-01 17:12:34 +0530

    fix: graphql error formatter

--- src/common/graphql/graphql-error-formatter.ts
+++ src/common/graphql/graphql-error-formatter.ts
@@ -1,9 +1,9 @@
 export class GqlErrorFormatter {
   public static formatError(error: any): any {
     return {
-      message: error.extensions.originalError?.message || error.message,
-      statusCode: error.extensions.originalError?.statusCode,
-      error: error.extensions.originalError.error,
+      message: error.extensions?.originalError?.message || error.message,
+      statusCode: error.extensions?.originalError?.statusCode,
+      error: error.extensions?.originalError?.error,
       path: error.path,
     };
   }
