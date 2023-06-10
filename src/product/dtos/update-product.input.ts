dc9ffe6ccddb6ca96276f8ca2bb5618e0635d336
Talwinder Singh <singhtalwinder790@gmail.com>
2023-06-10 14:45:26 +0530

    feat: add update product input

---
+++ src/product/dtos/update-product.input.ts
@@ -0,0 +1,7 @@
+import { InputType, PartialType } from '@nestjs/graphql';
+import { ProductInput } from './product.input';
+
+@InputType()
+export class UpdateProductInput extends PartialType(ProductInput) {
+
+}
