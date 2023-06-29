f735583e7afeb0199586894993061561350ad9e2
Talwinder Singh <singhtalwinder790@gmail.com>
2023-06-29 13:11:23 +0530

    feat: add isStockAvailable in inventory service

--- src/inventory/services/inventory.service.ts
+++ src/inventory/services/inventory.service.ts
@@ -3,11 +3,26 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { BaseService } from 'src/common';
 import { Inventory, InventoryDocument } from '../entities';
+import { OrderItemInput, PlaceOrderInput } from 'src/order/dto';
 
 @Injectable()
 export class InventoryService extends BaseService<InventoryDocument> {
   constructor(@InjectModel(Inventory.name) inventoryModel: Model<InventoryDocument>) {
     super(inventoryModel);
   }
+
+  public async isStockAvailable(order: PlaceOrderInput): Promise<boolean> {
+    const productIds: string[] = PlaceOrderInput.getProductIds(order);
+    const inventories: InventoryDocument[] = await this.find({ product: { $in: productIds }});
+    
+    return order.items.reduce((isStockAvailable: boolean, item: OrderItemInput) => {
+      const inventory: InventoryDocument = inventories.find(
+        (inventory: InventoryDocument) => inventory.product === item.productId,
+      );
+        
+      return isStockAvailable && inventory.getTotalQuantity() >= item.quantity;
+    }, true);
+  }
   
+
 }
