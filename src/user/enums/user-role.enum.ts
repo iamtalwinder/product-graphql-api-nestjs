5c6d00c3073553255c3e43777c1ba3d68c878e1e
Talwinder Singh <singhtalwinder790@gmail.com>
2023-07-14 10:12:04 +0530

    chore(doc): add doc for user role enum

--- src/user/enums/user-role.enum.ts
+++ src/user/enums/user-role.enum.ts
@@ -9,4 +9,19 @@ export enum UserRole {
 
 registerEnumType(UserRole, {
   name: 'UserRole',
+  description: 'Defines the role of a user within the system and their respective permissions.',
+  valuesMap: {
+    customer: {
+      description: 'A customer who can view and purchase products and view their orders.', 
+    },
+    admin: {
+      description: 'An administrator with full privileges, including managing users and roles.', 
+    },
+    manager: {
+      description: 'A manager with administrative capabilities, except for creating or managing admins or other managers.', 
+    },
+    anonymous: {
+      description: 'An anonymous user who can only view products but cannot make purchases.', 
+    },
+  },
 });
