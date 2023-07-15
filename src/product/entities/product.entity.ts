59e909c380d3dee8834788685fe0a6b1cc457520
Talwinder Singh <singhtalwinder790@gmail.com>
2023-07-15 07:01:02 +0530

    chore(doc): add doc for product entity

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -3,48 +3,46 @@ import { Field, ObjectType } from '@nestjs/graphql';
 import { Prop, SchemaFactory, Schema as SchemaDecorator } from '@nestjs/mongoose';
 import { Base } from 'src/common';
 
-@ObjectType()
+@ObjectType({ 
+  description: 'Represents a product in the system, including details like name, SKU, price, and category.',
+})
 @SchemaDecorator()
 export class Product extends Base {
-  @Field()
-  @Prop({
-    required: true,
-  })
+  @Field({ description: 'The name of the product.' })
+  @Prop({ required: true })
   name: string;
 
-  @Field()
+  @Field({ description: 'A unique stock keeping unit identifier for the product.' })
   @Prop({ required: true, unique: true })
   sku: string;
 
-  @Field({ nullable: true })
+  @Field({ nullable: true, description: 'An optional description of the product.' })
   @Prop()
   description?: string;
 
-  @Field()
-  @Prop({
-    required: true,
-  })
+  @Field({ description: 'The price of the product.' })
+  @Prop({ required: true })
   price: number;
 
-  @Field(() => String, { nullable: true })
+  @Field(() => String, { nullable: true, description: 'The category of the product. Optional.' })
   @Prop(String)
   category?: string;
 
-  @Field(() => [String], { nullable: 'itemsAndList' })
+  @Field(() => [String], { nullable: 'itemsAndList', description: 'A list of image URLs for the product. Optional.' })
   @Prop([String])
   images?: string[];
 
-  @Field(() => [String], { nullable: 'itemsAndList' })
+  @Field(() => [String], { nullable: 'itemsAndList', description: 'A list of tags associated with the product. Optional.' })
   @Prop([String])
   tags?: string[];
 
-  @Field()
+  @Field({ description: 'The date and time when the product was created.' })
   createdAt: Date;
 
-  @Field()
+  @Field({ description: 'The date and time when the product was last updated.' })
   updatedAt: Date;
 }
 
-export type ProductDocument =  Product & Document;
+export type ProductDocument = Product & Document;
 
 export const ProductSchema: Schema = SchemaFactory.createForClass(Product);
