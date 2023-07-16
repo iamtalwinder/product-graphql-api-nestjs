26c3a338b2f1db8cd2c39aa9aef8ac34212b6e66
Talwinder Singh <singhtalwinder790@gmail.com>
2023-07-16 11:13:54 +0530

    chore(doc): add doc for product resolver

--- src/product/resolvers/product.resolver.ts
+++ src/product/resolvers/product.resolver.ts
@@ -1,50 +1,75 @@
 import { Resolver, Query, Args, Mutation, Int } from '@nestjs/graphql';
 import { UseGuards } from '@nestjs/common';
 import { JwtAuthGuard, Roles, RolesGuard } from 'src/common';
+import { UserRole } from 'src/user';
 import { ProductService } from '../services/product.service';
 import { Product } from '../entities';
 import { CreateProductInput, GetProductsOutput, ProductFilterInput, UpdateProductInput } from '../dtos';
-import { UserRole } from 'src/user';
 
 @Resolver(() => Product)
 @UseGuards(JwtAuthGuard, RolesGuard)
 export class ProductResolver {
   constructor(private productService: ProductService) {}
 
-
-  @Query(() => GetProductsOutput)
+  /**
+   * Retrieves a list of products, with optional filtering, pagination, and limit.
+   */
+  @Query(() => GetProductsOutput, { description: 'Retrieve a list of products with optional filters.' })
   async getProducts(
-    @Args('filter', { type: () => ProductFilterInput, nullable: true }) filter: ProductFilterInput,
-    @Args('page', { type: () => Int, nullable: true, defaultValue: 1 }) page?: number,
-    @Args('limit', { type: () => Int, nullable: true, defaultValue: 20 }) limit?: number,
+    @Args('filter', { type: () => ProductFilterInput, nullable: true, description: 'Filter options for products.' })
+    filter: ProductFilterInput,
+    @Args('page', { type: () => Int, nullable: true, defaultValue: 1, description: 'Page number for pagination.' })
+    page?: number,
+    @Args('limit', { type: () => Int, nullable: true, defaultValue: 20, description: 'Number of products per page.' })
+    limit?: number,
   ): Promise<GetProductsOutput> {
     return this.productService.findAllWithFilterAndCount(filter, page, limit);
   }
 
-  @Query(() => Product)
-  async getProductById(@Args('id') id: string): Promise<Product> {
+  /**
+   * Retrieves a single product by its ID.
+   */
+  @Query(() => Product, { description: 'Retrieve a single product by its unique identifier.' })
+  async getProductById(
+    @Args('id', { description: 'The unique identifier of the product.' }) id: string,
+  ): Promise<Product> {
     return this.productService.findOneById(id);
   }
 
-  @Mutation(() => Product)
+  /**
+   * Creates a new product with the given details.
+   */
+  @Mutation(() => Product, { description: 'Create a new product.' })
   @Roles(UserRole.admin, UserRole.manager)
-  async createProduct(@Args('createProductInput') createProductInput: CreateProductInput): Promise<Product> {
+  async createProduct(
+    @Args('createProductInput', { description: 'The data used to create a new product.' })
+    createProductInput: CreateProductInput,
+  ): Promise<Product> {
     return this.productService.create(createProductInput);
   }
 
-  @Mutation(() => Product)
+  /**
+   * Updates an existing product identified by its ID with new details.
+   */
+  @Mutation(() => Product, { description: 'Update an existing product.' })
   @Roles(UserRole.admin, UserRole.manager)
   async updateProduct(
-    @Args('id') id: string,
-    @Args('updateProductInput') updateProductInput: UpdateProductInput,
+    @Args('id', { description: 'The unique identifier of the product to be updated.' }) id: string,
+    @Args('updateProductInput', { description: 'The new details to update the product with.' })
+    updateProductInput: UpdateProductInput,
   ): Promise<Product> {
     return this.productService.findOneAndUpdate(id, updateProductInput);
   }
 
-  @Mutation(() => Boolean)
+  /**
+   * Deletes a product identified by its ID.
+   */
+  @Mutation(() => Boolean, { description: 'Delete a product.' })
   @Roles(UserRole.admin, UserRole.manager)
-  async deleteProduct(@Args('id') id: string): Promise<boolean> {
+  async deleteProduct(
+    @Args('id', { description: 'The unique identifier of the product to be deleted.' }) id: string,
+  ): Promise<boolean> {
     await this.productService.deleteById(id);
-    return true
+    return true;
   }
 }
